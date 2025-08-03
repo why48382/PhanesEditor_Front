c09ab9b0918f0dc63c1949e95d15c365caf2f4cf
LEEPC <why48382@gmail.com>
2025-08-03 14:58:41 +0900

    monaco editor 불러오는 방식 변경
    이제 getValue()를 통해 정상적으로 에디터 안에 값을 가져올 수 있음

--- phanesVueLee/src/components/project/Editor.vue
+++ phanesVueLee/src/components/project/Editor.vue
@@ -1,37 +1,42 @@
 <script setup>
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-
-self.MonacoEnvironment = {
-    getWorker(_, label) {
-        if (label === 'json') return new jsonWorker();
-        if (['css', 'scss', 'less'].includes(label)) return new cssWorker();
-        if (['html', 'handlebars', 'razor'].includes(label)) return new htmlWorker();
-        if (['typescript', 'javascript'].includes(label)) return new tsWorker();
-        return new editorWorker();
-    }
-};
-
-import * as monaco from 'monaco-editor';
-import { onMounted, ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import loader from '@monaco-editor/loader';
 
 const editorContainer = ref(null);
+let editorInstance = null;
 
 onMounted(() => {
-    monaco.editor.create(editorContainer.value, {
-        value: 'console.log("Hello World")\n',
-        language: 'javascript',
-        theme: 'vs-dark',
-        automaticLayout: true
+    loader.init().then(monaco => {
+        editorInstance = monaco.editor.create(editorContainer.value, {
+            value: '',
+            language: 'javascript',
+            theme: 'vs-dark',
+            automaticLayout: true
+        });
+        editorInstance.onDidChangeModelContent((event) => {
+                const fullText = editorInstance.getValue();  // 전체 코드
+                console.log('현재 텍스트:', fullText);
+                // 뭔가 라이브러리가 덜 받아진것 같은 느낌 아니면 덜 적용 되었거나.
+                event.changes.forEach(change => {
+                    console.log('입력된 텍스트:', change.text);
+                    console.log('변경 범위:', change.range);
+                });
+            });
     });
+});
+
+onBeforeUnmount(() => {
+    if (editorInstance) editorInstance.dispose();
 });
 </script>
 
 <template>
-    <div ref="editorContainer" style="width:80%; height:100%;"></div>
+    <div ref="editorContainer" class="editor-container"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.editor-container {
+    width: 100%;
+    height: 100%;
+}
+</style>
