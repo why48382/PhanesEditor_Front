6c8cd32c562169dda62e1dc913c228716af16dfe
염준선 <wnstjs1031@naver.com>
2025-08-29 13:48:09 +0900

    [Fix] 로그아웃 기능
    프론트에서 자체 처리를 위한 주석처리
    추후에 백엔드 통신으로 토큰 없애도록 처리예정

--- phanesVueLee/src/api/user/user_index.js
+++ phanesVueLee/src/api/user/user_index.js
@@ -31,20 +31,20 @@ const userSignIn = async (req) => {
     return data;
 }
 
-const logOut = async () => {
-    let data = {};
-    let url = '/api/v1/user/usr_logout.json';
+// const logOut = async () => {
+//     let data = {};
+//     let url = '/api/v1/user/usr_logout.json';
 
-    await api.get(url)
-        .then((res) => {
-            data = res.data;
-        })
-        .catch((error) => {
-            data = error.data;
-        });
+//     await api.get(url)
+//         .then((res) => {
+//             data = res.data;
+//         })
+//         .catch((error) => {
+//             data = error.data;
+//         });
 
-    return data;
-}
+//     return data;
+// }
 
 // 벡엔드 호출 후 받은 데이터를 반환
 
