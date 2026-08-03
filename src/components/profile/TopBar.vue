233cffff9d7abc16ef6976d0e9691cf2594a087c
why4832 <why48382@gmail.com>
2026-08-03 12:56:09 +0900

    refactor: 임시 프로필 이미지 보이지 않게 변경

--- src/components/profile/TopBar.vue
+++ src/components/profile/TopBar.vue
@@ -1,21 +1,19 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 
-
 </script>
 
 <template>
     <div class="top-bar">
         <h1>
-
             <RouterLink :to="{ name: 'main' }">
                 Phanes
             </RouterLink>
         </h1>
-        <div class="profile-icon">
-            <img src="https://lh3.googleusercontent.com/a/ACg8ocIYJXGITMJfJ8BKyRNsFD3n3RPixBPQSWutwuhHxb44cBr74cFC=s288-c-no"
-                class="profile-img" alt="임시 이미지">
-        </div>
+<!--        <div class="profile-icon">-->
+<!--            <img src="https://lh3.googleusercontent.com/a/ACg8ocIYJXGITMJfJ8BKyRNsFD3n3RPixBPQSWutwuhHxb44cBr74cFC=s288-c-no"-->
+<!--                class="profile-img" alt="프로필 이미지">-->
+<!--        </div>-->
     </div>
 </template>
 
